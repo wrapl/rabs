5099e644694680bdda3667a0f4137feba4b0ead7
Raja Mukherji <rajamukherji@gmail.com>
2021-10-05 09:12:25 +0100

    Updates.

--- src/rabs.h
+++ src/rabs.h
@@ -29,7 +29,7 @@ extern __thread target_t *CurrentTarget;
 
 ml_value_t *rabs_global(const char *Name);
 
-#define CURRENT_VERSION 2, 20, 1
+#define CURRENT_VERSION 2, 20, 2
 #define WORKING_VERSION 2, 10, 0
 
 #endif
